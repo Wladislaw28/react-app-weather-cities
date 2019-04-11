2c38dc0cf849c3dc88f7ed0ee27dc5332df3ceaa
Владислав <40837008+Wladislaw28@users.noreply.github.com>
2019-04-11 14:23:49 +0300

    Updates

---
+++ precache-manifest.ac17b21b9cb7053884eca9aac62748bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "653d0b855718d58a51b57c11b438fa48",
+    "url": "/react-app-weather-cities/static/media/2.653d0b85.jpeg"
+  },
+  {
+    "revision": "69cbebbf6a63cdc476e7ce78fc9cfa7d",
+    "url": "/react-app-weather-cities/static/media/1.69cbebbf.jpeg"
+  },
+  {
+    "revision": "0e43a3a3c37fb72a47ec",
+    "url": "/react-app-weather-cities/static/js/runtime~main.0e43a3a3.js"
+  },
+  {
+    "revision": "7501eec28636ac8cdbea",
+    "url": "/react-app-weather-cities/static/js/main.7501eec2.chunk.js"
+  },
+  {
+    "revision": "2c5602024c4a2b0c234e",
+    "url": "/react-app-weather-cities/static/js/2.2c560202.chunk.js"
+  },
+  {
+    "revision": "7501eec28636ac8cdbea",
+    "url": "/react-app-weather-cities/static/css/main.7cc72091.chunk.css"
+  },
+  {
+    "revision": "2c5602024c4a2b0c234e",
+    "url": "/react-app-weather-cities/static/css/2.ea604465.chunk.css"
+  },
+  {
+    "revision": "d1958111ff1e32cdefcf60767032b3da",
+    "url": "/react-app-weather-cities/index.html"
+  }
+];
